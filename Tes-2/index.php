a2ced1ffbb2659a0c8c3539cfc6430eec4ec111f
fide07 <imanuelfide@gmail.com>
2021-03-21 22:26:23 +0700

    treasure hunter tes
    Task #2

---
+++ Tes-2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <script defer src="script.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="styles.css">
+    
+    <title>Maze Game Deluxe</title>
+</head>
+
+<body>
+	
+  <div id='cover'>
+<table>
+
+</table>
+</div>
+<?php 
+ echo "<button id = 'one' type='button' name='Click to Play' onClick = 'loadPage();'>Click to Play</button>";
+echo "<button id = 'two' type='button' name='controls' onClick = 'info();'>Click for Controls</button>";
+?>
+</body>
+
+</html>
